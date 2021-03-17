ba3e2c831d11b15de7303bc9538b168e8ab75a01
Shaik Imranuddin <78163908+skimran2000@users.noreply.github.com>
2021-03-17 23:00:28 +0530

    Delete NextGen directory

--- NextGen/inc/footer.inc.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>@<a href="#">NextGen Technologies</a></b>
-    </div>
-    <strong>&emsp;Copyright &copy; 2021 HRM, Finance Department</strong>
-</footer>
